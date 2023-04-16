ea7e9ff0d8b97c91a9b3285bd05c6be4521f6b8e
Boris <D:\The Bat!>
2023-04-16 09:14:09 +0300

    ex002

---
+++ ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿// third figure
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+if(N>=1000)
+{
+    while(N>=1000)
+    {
+        N=N/10;
+    }
+N=N%10;
+Console.Write($"Третья цифра числа равна  {N}");
+}
+else
+Console.Write("Третьей цифры нет");
